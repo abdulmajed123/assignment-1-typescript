ac0ff06f8bebe6a45142fef1e96a2a671ba49458
abdulmajed123 <abdulmajed5618@gmail.com>
2026-05-07 18:33:08 +0600

    solve problem 7: add array intersection function

--- src/solutions.ts
+++ src/solutions.ts
@@ -91,3 +91,12 @@ class Student extends Person {
 const student = new Student("Alice", 20, "A");
 const output = student.getDetails();
 console.log(output);
+
+// Problem No: 7
+
+function getIntersection(arr1: number[], arr2: number[]): number[] {
+  return arr1.filter((num) => arr2.includes(num));
+}
+
+const result4 = getIntersection([1, 2, 3, 4, 5], [3, 4, 5, 6, 7]);
+console.log(result4);
